156d221f5d28053bbb377e575b314759d229ce6e
Evan Hahn <me@evanhahn.com>
2015-04-12 22:55:28 -0400

    Throw an error if the collection name contains a separator

--- test/errors.js
+++ test/errors.js
@@ -1,6 +1,7 @@
 var expect = require('expect.js');
 
 var aubrey = require('..');
+var ObjectStore = require('aubrey-object-store');
 
 describe('error detection', function () {
   describe('misconfiguration', function () {
@@ -14,6 +15,13 @@ describe('error detection', function () {
 
     it('throws an error without a store configuration', function () {
       expect(create).withArgs({}).to.throwError();
+    });
+
+    it('throws an error if the name has the separator in it', function () {
+      expect(create).withArgs({
+        name: 'what===the',
+        store: new ObjectStore()
+      }).to.throwError();
     });
   });
 });
