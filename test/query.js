0865b1d3f9ea49cac0a56fe779f0a962cff36e46
Evan Hahn <me@evanhahn.com>
2015-04-12 23:23:59 -0400

    add hecka tests for Query

--- test/query.js
+++ test/query.js
@@ -19,8 +19,64 @@ describe('query', function () {
   });
 
   it('gets all of the elements by default', function (done) {
-    this.query.exec((err, values) => {
+    this.query.find((err, values) => {
       expect(values).to.eql(this.people);
+      done(err);
+    });
+  });
+
+  it('can limit the number of values', function (done) {
+    this.query
+    .limit(2)
+    .find((err, values) => {
+      expect(this.people).to.contain(values[0]);
+      expect(this.people).to.contain(values[2]);
+      done(err);
+    });
+  });
+
+  it('can select a single element with one criteria', function (done) {
+    this.query
+    .where('first', 'equals', 'Ron')
+    .find((err, values) => {
+      expect(values).to.eql([this.people[0]]);
+      done(err);
+    });
+  });
+
+  it('can select a multiple elements with one criteria', function (done) {
+    this.query
+    .where('gender', 'equals', 'male')
+    .find((err, values) => {
+      expect(values).to.have.length(2);
+      expect(values).to.contain([this.people[0]]);
+      expect(values).to.contain([this.people[2]]);
+      done(err);
+    });
+  });
+
+  it('can select one element with multiple criteria', function (done) {
+    this.query
+    .where('anchor', 'equals', true)
+    .where('gender', 'equals', 'female')
+    .find((err, values) => {
+      expect(values).to.eql([this.people[1]]);
+      done(err);
+    });
+  });
+
+  it('can find just one value', function (done) {
+    this.query.findOne((err, value) => {
+      expect(this.people).to.contain(value);
+      done(err);
+    });
+  });
+
+  it('can find one element matching a criteria', function (done) {
+    this.query
+    .where('gender', 'equals', 'male')
+    .findOne((err, value) => {
+      expect([this.people[0], this.people[2]]).to.contain(value);
       done(err);
     });
   });
