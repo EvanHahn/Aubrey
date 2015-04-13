eb4b8ddb9cd74d4ba57f4d08f7434e80ef25f15e
Evan Hahn <me@evanhahn.com>
2015-04-12 22:32:48 -0500

    Simple tests for package.json

---
+++ test/package.js
@@ -0,0 +1,15 @@
+const expect = require('expect.js');
+
+const pkg = require('../package.json');
+
+describe.only('package dot json', function () {
+  `
+    name description author version main license
+    keywords repository bugs
+  `.trim().split(/\s+/).forEach(function (property) {
+
+    it('has ' + property, function () {
+      expect(pkg[property]).not.to.be(undefined);
+    });
+  });
+});
